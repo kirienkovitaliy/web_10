59d2402e839a236fc92dcdbcb79951e16b0b4b54
kirienkovitaliy <kirienkovitaliy1989@gmail.com>
2023-06-04 14:01:13 +0300

    create

---
+++ hw_project/utils/migration.py
@@ -0,0 +1,43 @@
+import os
+import django
+
+from pymongo import MongoClient
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "hw_project.settings")
+django.setup()
+
+from quotes.models import Quote, Tag, Author  # noqa
+
+client = MongoClient("mongodb://localhost")
+
+db = client.hw
+
+authors = db.authors.find()
+
+for author in authors:
+    Author.objects.get_or_create(
+        fullname=author['fullname'],
+        date_born=author['date_born'],
+        location_born=author['location_born'],
+        bio=author['bio']
+    )
+
+quotes = db.quotes.find()
+
+for quote in quotes:
+    tags = []
+    for tag in quote['tags']:
+        t, *_ = Tag.objects.get_or_create(name=tag)
+        tags.append(t)
+
+    exist_quote = bool(len(Quote.objects.filter(quote=quote['quote'])))
+
+    if not exist_quote:
+        author = db.authors.find_one({'_id': quote['author']})
+        a = Author.objects.get(fullname=author['fullname'])
+        q = Quote.objects.create(
+            quote=quote['quote'],
+            author=a
+        )
+        for tag in tags:
+            q.tags.add(tag)
